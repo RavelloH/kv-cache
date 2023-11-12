c8f7226bf59222c6fd5852ac4a9fd138037a96c7
RavelloH <hyh20060327@qq.com>
2023-11-12 12:36:49 +0800

    Dev. 增加日志输出

--- api/main.js
+++ api/main.js
@@ -155,6 +155,7 @@ const readData = async (req) => {
     }
 
     // 检测IP验证
+    console.log(storedData.safeIP, req.headers["x-real-ip"])
     if (!checkIP(storedData.safeIP, req.headers["x-real-ip"] ||
         req.headers['x-forwarded-for'] ||
         req.ip ||
