8cfe437d93a438d55fa41b51f31b50355990a834
RavelloH <hyh20060327@qq.com>
2023-11-18 22:45:50 +0800

    Feat. 对预检提供支持

--- api/main.js
+++ api/main.js
@@ -255,10 +255,9 @@ export default async function handler(req, res) {
                 version: '1.0.0',
                 active: dbsize
             });
-        } else {
-            res.status(405).json({
-                code: 405,
-                message: "不允许的请求方法"
+        } else if (req.method === "OPTIONS"){
+            res.status(200).json({
+                code: 200
             });
         }
     } catch (error) {
